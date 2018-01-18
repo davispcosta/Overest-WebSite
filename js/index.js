8263e3214130542b05d3959864f30d33082d15e1
AdrianoASJ <adrianosousa.junior@gmail.com>
2018-01-18 01:02:25 -0200

    just little changes

--- js/index.js
+++ js/index.js
@@ -420,7 +420,7 @@ var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
+  this.period = parseInt(period, 7) || 2000;
   this.txt = '';
   this.tick();
   this.isDeleting = false;
@@ -439,7 +439,7 @@ TxtRotate.prototype.tick = function() {
   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
   var that = this;
-  var delta = 300 - Math.random() * 100;
+  var delta = 200 - Math.random() * 100;
 
   if (this.isDeleting) { delta /= 2; }
 
